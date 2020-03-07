33d25d2199155b7c4d8a58f03052fb75752c9b32
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2020-03-07 12:22:34 +0600

    Модифицирован index.js для подключения основного хранилища и роутера истории переходов в браузере.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import "react-notifications/lib/notifications.css";
 import * as serviceWorker from './serviceWorker';
+import {ConnectedRouter} from "connected-react-router";
+import {Provider} from "react-redux";
+import {store, history} from "./store/configureStore";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <App/>
+        </ConnectedRouter>
+    </Provider>
+);
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
