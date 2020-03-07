362554059c6fc9b14ee128109918e212e5349d9e
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2020-03-07 12:20:05 +0600

    Созданы основные контенйеры проекта и файлы для работы с redux.

---
+++ src/containers/AddPicture/AddPicture.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class AddPicture extends Component {
+    render() {
+        return (
+            <div>AddPicture</div>
+        );
+    }
+}
+
+export default AddPicture;
